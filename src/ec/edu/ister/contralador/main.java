4f126bbbc71923d185e6959dcaa6cafecd830e75
Alexanderjun <alexander1995mh@hotmail.com>
2019-03-01 01:45:13 -0500

    Examen POO ll

---
+++ src/ec/edu/ister/contralador/main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ister.contralador;
+
+import ec.edu.ister.modelo.Pedido;
+import ec.edu.ister.modelo.SolParcial;
+
+/**
+ *
+ * @author Alexander
+ */
+public class main {
+    
+    public static void main(String[] args) {
+        
+        
+        
+        
+    }
+    
+}
